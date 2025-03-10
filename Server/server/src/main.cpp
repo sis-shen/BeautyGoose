416a63f1950da39ee78f4e5e3e7f5ba780275705
supdriver <adongfa@gmail.com>
2025-03-10 11:35:04 +0800

    添加spdlog并完成初始化

--- Server/server/src/main.cpp
+++ Server/server/src/main.cpp
@@ -1,4 +1,5 @@
 #include "HTTPServer.hpp"
+#include "logger.hpp"
 #include <gflags/gflags.h>
 
 DEFINE_string(host,"0.0.0.0","服务器监听的源ip地址范围");
@@ -10,9 +11,15 @@ DEFINE_string(db_host,"localhost","数据库服务所在的ip");
 DEFINE_string(db_port,"3306","数据库服务开放的端口");
 DEFINE_string(db_database,"db","数据库中存储数据的数据库");
 
+DEFINE_string(log_logfile,"./log/logfile","日志落地文件及路径");
+DEFINE_bool(log_release_mode,false,"设置是否为发布模式输出日志,\ntrue:过滤低等级日志，输出到文件\nfalse:输出所有日志，输出到标准输出");
+DEFINE_int32(log_release_level,spdlog::level::warn,"设置release模式下过滤的日志等级");
+
 int main(int argc, char *argv[])
 {
     google::ParseCommandLineFlags(&argc,&argv,true);//解析命令行参数
+    btyGoose::init_logger(FLAGS_log_release_mode,FLAGS_log_logfile,FLAGS_log_release_level);//初始化日志器
+    LOG_DEBUG("全局日志器初始化成功");
     btyGoose::HTTPServer::getInstance()->initDB(FLAGS_db_user,FLAGS_db_password,FLAGS_db_host,FLAGS_db_port,FLAGS_db_database);
     btyGoose::HTTPServer::getInstance()->init(FLAGS_host,FLAGS_port);
     btyGoose::HTTPServer::getInstance()->start();
