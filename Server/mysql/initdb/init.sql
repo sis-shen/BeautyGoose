a788a7890593d34ea087cedded1245ab1a8d045c
supdriver <adongfa@gmail.com>
2025-03-23 21:49:22 +0800

    修改数据表

--- Server/mysql/initdb/init.sql
+++ Server/mysql/initdb/init.sql
@@ -12,69 +12,75 @@ flush privileges;
 use BeautyGoose;
 
 create table account(
-    uuid varchar(13) unique,
-    phone varchar(12) unique,
-    name varchar(10) not null,
-    password varchar(64) not null,
-    nickname varchar(32),
-    level tinyint not null default 0,
-    icon_data blob,
-    type tinyint not null default 0,
-    balance int unsigned default 0
-);
+    `id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `uuid` varchar(32) unique,
+    `phone` varchar(16) unique,
+    `name` varchar(16) not null,
+    `password` varchar(64) not null,
+    `nickname` varchar(32),
+    `icon_data` blob,
+    `level` tinyint not null default 0,
+    `type` tinyint not null default 0,
+    `balance` decimal unsigned default 0 
+)ENGINE=InnoDB;
 
 create table dishes(
-    uuid varchar(13) unique,
-    merchant_id varchar(13) not null,
-    merchant_name varchar(10) not null,
-    name varchar(10) not null,
-    icon_path varchar(150),
-    description text,
-    base_price decimal(6,2),
-    price_factor float(4,2) unsigned,
-    is_delete boolean not null default 1
-);
+    `id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `uuid` varchar(32) unique,
+    `merchant_id` varchar(32) not null,
+    `merchant_name` varchar(16) not null,
+    `name` varchar(16) not null,
+    `icon_path` varchar(256),
+    `description` text,
+    `base_price` decimal(6,2),
+    `price_factor` float(4,2) unsigned,
+    `is_delete` boolean not null default 1
+)ENGINE=InnoDB;
 
 create table orders(
-    time varchar(32) not null, 
-    uuid varchar(13) unique,
-    merchant_id varchar(13) not null,
-    merchant_name varchar(10) not null,
-    consumer_id varchar(13) not null,
-    consumer_name varchar(10) not null,
-    level tinyint not null default 0,
-    pay decimal(10,2) not null default 0,
-    status tinyint not null default 0,
-    sum int not null
-);
+    `id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `time` varchar(32) not null, 
+    `uuid` varchar(32) unique,
+    `merchant_id` varchar(32) not null,
+    `merchant_name` varchar(16) not null,
+    `consumer_id` varchar(32) not null,
+    `consumer_name` varchar(16) not null,
+    `level` tinyint not null default 0,
+    `pay` decimal(10,2) not null default 0,
+    `status` tinyint not null default 0,
+    `sum` int not null
+)ENGINE=InnoDB;
 
 create table orderDish(
-    order_id varchar(13) not null,
-    dish_id varchar(13) not null,
-    merchant_id varchar(13) not null,
-    name varchar(10) not null,
-    dish_price decimal(6,2),
-    count int not null default 0
-);
+    `id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `order_id` varchar(32) not null,
+    `dish_id` varchar(32) not null,
+    `merchant_id` varchar(32) not null,
+    `name` varchar(16) not null,
+    `dish_price` decimal(6,2),
+    `count` int not null default 0
+)ENGINE=InnoDB;
 
 create table history(
-    time varchar(32) not null, 
-    uuid varchar(13) unique,
-    merchant_id varchar(13) not null,
-    merchant_name varchar(10) not null,
-    consumer_id varchar(13) not null,
-    consumer_name varchar(10) not null,
-    level tinyint not null default 0,
-    pay decimal(10,2) not null default 0,
-    status tinyint not null default 0,
-    sum int not null
-);
+    `id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `time` varchar(32) not null, 
+    `uuid` varchar(32) unique,
+    `merchant_id` varchar(32) not null,
+    `merchant_name` varchar(16) not null,
+    `consumer_id` varchar(32) not null,
+    `consumer_name` varchar(16) not null,
+    `level` tinyint not null default 0,
+    `pay` decimal(10,2) not null default 0,
+    `status` tinyint not null default 0,
+    `sum` int not null
+)ENGINE=InnoDB;
 
 create table historyDish(
-    order_id varchar(13) not null,
-    dish_id varchar(13) not null,
-    merchant_id varchar(13) not null,
-    name varchar(10) not null,
-    dish_price decimal(6,2),
-    count int not null default 0
-);
+    `id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `order_id` varchar(32) not null,
+    `dish_id` varchar(32) not null,
+    `merchant_id` varchar(32) not null,
+    `name` varchar(16) not null,
+    `dish_price` decimal(6,2),
+    `count` int not null default 0
+)ENGINE=InnoDB;
