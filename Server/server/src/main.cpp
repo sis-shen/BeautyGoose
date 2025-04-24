c3a2fbb36c33d8a785463e66ba2449767da52f5c
sis-shen <adongfa@gmail.com>
2025-04-24 20:00:04 +0800

    适配drogon

--- Server/server/src/main.cpp
+++ Server/server/src/main.cpp
@@ -1,4 +1,4 @@
-#include "HTTPServer.hpp"
+#include "./controllers/btyGoose_TestCtrl.h"
 #include "logger.hpp"
 #include <gflags/gflags.h>
 #include <sw/redis++/redis.h>
@@ -23,6 +23,7 @@ DEFINE_bool(redis_keep_alive,true,"是否保持长连接");
 DEFINE_string(redis_master_password,"password","redis主节点的登录密码");
 
 //日志器相关参数
+DEFINE_string(log_drogon_logdir,"./logs/drogon","drogon日志的输出目录");
 DEFINE_string(log_logfile,"./log/logfile","日志落地文件及路径");
 DEFINE_bool(log_release_mode,false,"设置是否为发布模式输出日志,\ntrue:过滤低等级日志，输出到文件\nfalse:输出所有日志，输出到标准输出");
 DEFINE_int32(log_release_level,spdlog::level::warn,"设置release模式下过滤的日志等级");
@@ -31,23 +32,29 @@ int main(int argc, char *argv[])
 {
     google::ParseCommandLineFlags(&argc,&argv,true);//解析命令行参数
     btyGoose::init_logger(FLAGS_log_release_mode,FLAGS_log_logfile,FLAGS_log_release_level);//初始化日志器
-    LOG_DEBUG("全局日志器初始化成功");
+    SUP_LOG_DEBUG("全局日志器初始化成功");
 
     auto db = std::make_shared<btyGoose::DatabaseClient>();
     db->init(FLAGS_db_user,FLAGS_db_password,FLAGS_db_host,FLAGS_db_port,FLAGS_db_database,                         
         std::chrono::seconds(FLAGS_db_reconnect_interval));
-    btyGoose::HTTPServer::getInstance()->setDB(db);
-    LOG_DEBUG("数据库对象设置成功");
+    btyGoose::SR::ins().setDB(db);
+    SUP_LOG_DEBUG("数据库对象设置成功");
 
     auto redis = std::make_shared<btyGoose::RedisClient>(
         btyGoose::RedisClient(FLAGS_redis_host,FLAGS_redis_port,FLAGS_redis_db,FLAGS_redis_keep_alive,FLAGS_redis_master_password));
     if(redis->isOK())
-        LOG_INFO("Redis连接成功!");
+        SUP_LOG_INFO("Redis连接成功!");
     else
-        LOG_ERROR("Redis连接失败!");
-    btyGoose::HTTPServer::getInstance()->setRedis(redis);
+        SUP_LOG_ERROR("Redis连接失败!");
+    btyGoose::SR::ins().setRedis(redis);
 
-    btyGoose::HTTPServer::getInstance()->init(FLAGS_host,FLAGS_port);
-    btyGoose::HTTPServer::getInstance()->start();
+    //启动服务器
+    SUP_LOG_INFO("drogon日志路径: {}",FLAGS_log_drogon_logdir);
+    app().setLogPath(FLAGS_log_drogon_logdir)
+    .setLogLevel(trantor::Logger::kWarn)
+    .addListener(FLAGS_host, FLAGS_port)
+    .setThreadNum(16)
+    // .enableRunAsDaemon()
+    .run();
     return 0;
 }
