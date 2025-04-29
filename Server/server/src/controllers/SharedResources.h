750e3598b0b64478734216aab80133f13dc95ee7
sis-shen <adongfa@gmail.com>
2025-04-29 11:10:10 +0800

    新增接口

--- Server/server/src/controllers/SharedResources.h
+++ Server/server/src/controllers/SharedResources.h
@@ -32,6 +32,28 @@ namespace btyGoose
             _redis = ptr;
         }
 
+        bool checkRedisOrderListExist(const data::Order &order)
+        {
+            bool has_consumer = _redis->hasOrderListByUserId(order.consumer_id);
+            bool has_merchant = _redis->hasOrderListByUserId(order.merchant_id);
+
+            if (has_consumer && has_merchant)
+                return true;
+            if (!has_consumer)
+            {
+                auto order_list = _db->getOrderListByConsumer(order.consumer_id);
+                _redis->setOrderList(order_list);
+                _redis->setOrderListByIdDone(order.consumer_id);
+            }
+            if (!has_merchant)
+            {
+                auto dish_list = _db->getOrderListByMerchant(order.merchant_id);
+                _redis->setOrderList(dish_list);
+                _redis->setOrderListByIdDone(order.merchant_id);
+            }
+            return false;
+        }
+
         const std::shared_ptr<DatabaseClient> db(){return _db;}
         const std::shared_ptr<RedisClient> redis(){return _redis;}
     private:
