39ebeb10c36ed5dd8edd6b0b6ec391e1ef17ef2d
sis-shen <adongfa@gmail.com>
2025-05-29 16:58:11 +0800

    添加文档注释

--- Server/server/src/controllers/SharedResources.h
+++ Server/server/src/controllers/SharedResources.h
@@ -1,3 +1,11 @@
+/**
+ * @file SharedResources.h
+ * @author supdriver
+ * @date 2025-05-26
+ * @brief 管理共享资源
+ * @version 2.6.0
+ */
+
 #pragma once
 #include "../DatabaseClient.hpp"
 #include "../RedisClient.hpp"
@@ -8,6 +16,11 @@
 namespace btyGoose
 {
     //SharedResources
+    /**
+     * @class SR
+     * @brief SharedResources共享资源类
+     * @details 用于维护业务所共享的资源，如MySQL连接资源，Redis连接资源等
+     */
     class SR
     {
     private:
@@ -15,23 +28,39 @@ namespace btyGoose
         SR(const SR&) =delete;
         SR operator=(const SR&) = delete;
     public:
+        /**
+         * @brief 单例模式，获取单例对象
+         * @return 单例对象
+         */
         static SR& ins()
         {
             static SR sr;
             return sr;
         }
 
+        /**
+         * @brief 设置数据库连接对象
+         * @param ptr 数据库连接对象的智能指针
+         */
         void setDB(std::shared_ptr<DatabaseClient>& ptr)
         {
             // LOG_TRACE("HTTPServer: 开始初始化db");
             _db = ptr;
         }
 
+        /**
+         * @brief 设置Redis连接对象
+         * @param ptr Redis连接对象的智能指针
+         */
         void setRedis(std::shared_ptr<RedisClient>&ptr)
         {
             _redis = ptr;
         }
 
+        /**
+         * @brief 检测订单对应的商家订单列表和消费者订单列表是否存在
+         * @param order 订单对象
+         */
         bool checkRedisOrderListExist(const data::Order &order)
         {
             bool has_consumer = _redis->hasOrderListByUserId(order.consumer_id);
